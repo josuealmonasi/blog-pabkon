368cabb487d7f03980a7e3bb0be16a2a6071e269
Josué Almonasi <josue.almonasi@hotmail.com>
2023-01-11 20:45:48 -0600

    Added blog path

--- pages/blog/[entry].tsx
+++ pages/blog/[entry].tsx
@@ -30,14 +30,10 @@ export const getStaticPaths: GetStaticPaths = async _ctx => {
     content_type: 'blogPost-2',
   });
 
-  console.log(response);
-
   const paths = response.items.map(item => ({
     //@ts-ignore
     params: { entry: item.fields.slug },
   }));
-
-  console.log(paths);
 
   return {
     paths,
@@ -48,7 +44,6 @@ export const getStaticPaths: GetStaticPaths = async _ctx => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params as { slug: string };
-  console.log(slug);
 
   const response = await client.getEntries({
     content_type: 'blogPost-2',
